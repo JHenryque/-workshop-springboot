e431e80930ec76dfd730f147a5276db2e6ab372c
JHenryque <henryque.pc.game@gmail.com>
2024-01-19 22:57:41 -0300

    iniciano sprongboot

---
+++ src/test/java/org/projeto/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projeto.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
